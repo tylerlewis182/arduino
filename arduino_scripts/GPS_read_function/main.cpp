35e945512f02688c1f883ff6739d9f7ee0cd49a3
Tyler Lewis <tylerlewis182@gmail.com>
2019-04-12 00:58:51 -0500

    cleaned up main.cpp

--- arduino_scripts/GPS_read_function/main.cpp
+++ arduino_scripts/GPS_read_function/main.cpp
@@ -12,7 +12,69 @@ const long MONITOR_BAUDRATE = 115200; // bits per second
 
 // timer for debugging
 uint32_t timer = millis();
-uint32_t counter = 0;
+//uint32_t counter = 0;
+
+
+// functions
+void update_gps()
+{
+  // read data from the GPS in the 'main loop'
+  char c = GPS.read();
+
+  if (GPS.newNMEAreceived())
+  {
+    // print the las recieved NMEA sentence
+    //Serial.println(GPS.lastNMEA()); // this sets the newNMEAreceived() flag to false
+    if (!GPS.parse(GPS.lastNMEA()))   // this sets the newNMEAreceived() flag to false
+    {
+      return;
+    }
+  }  
+}
+
+void print_gps()
+{
+  // approximately every 2 seconds or so, print out the current stats
+  //timer = millis(); // reset the timer
+  //Serial.print("Counter = ");
+  //Serial.println(counter);
+  //counter = 0; // reset the counter
+  Serial.print("\nTime: ");
+  Serial.print(GPS.hour, DEC); Serial.print(':');
+  Serial.print(GPS.minute, DEC); Serial.print(':');
+  Serial.print(GPS.seconds, DEC); Serial.print('.');
+  Serial.println(GPS.milliseconds);
+  Serial.print("Date: ");
+  Serial.print(GPS.day, DEC); Serial.print('/');
+  Serial.print(GPS.month, DEC); Serial.print("/20");
+  Serial.println(GPS.year, DEC);
+  Serial.print("Fix: "); Serial.print((int)GPS.fix);
+  Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
+  if (GPS.fix) 
+  {
+    Serial.print("Location: ");
+    Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+    Serial.print(", ");
+    Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+    Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+    Serial.print("Angle: "); Serial.println(GPS.angle);
+    Serial.print("Altitude: "); Serial.println(GPS.altitude);
+    Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+  }
+}
+
+void check_type(int a)
+{
+  Serial.println("it's an int");
+}
+
+void print_lat()
+{
+  //check_type(GPS.latitude);
+  Serial.println(GPS.latitude);
+}
+
+
 
 // setup
 void setup()
@@ -36,51 +98,23 @@ void setup()
 void loop()
 {
   // read data from the GPS in the 'main loop'
-  char c = GPS.read();
-  counter++; // count how many times the read() method is called
+  update_gps();
+  //counter++; // count how many times the read() method is called (for debugging)
 
-  if (GPS.newNMEAreceived())
+
+  // print gps data every few seconds
+  if(timer > millis()) // if timer rolls over, reset it
   {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
-    Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
-    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
-    return; // we can fail to parse a sentence in which case we should just wait for another
-  }  
-
-  // if millis() or timer wraps around, we'll just reset it
-  if (timer > millis()) timer = millis();
-
-  // approximately every 2 seconds or so, print out the current stats
-  if (millis() - timer > 2000) {
-    timer = millis(); // reset the timer
-    Serial.print("Counter = ");
-    Serial.println(counter);
-    counter = 0; // reset the counter
-    Serial.print("\nTime: ");
-    Serial.print(GPS.hour, DEC); Serial.print(':');
-    Serial.print(GPS.minute, DEC); Serial.print(':');
-    Serial.print(GPS.seconds, DEC); Serial.print('.');
-    Serial.println(GPS.milliseconds);
-    Serial.print("Date: ");
-    Serial.print(GPS.day, DEC); Serial.print('/');
-    Serial.print(GPS.month, DEC); Serial.print("/20");
-    Serial.println(GPS.year, DEC);
-    Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
-    if (GPS.fix) 
-    {
-      Serial.print("Location: ");
-      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-      Serial.print(", ");
-      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-      Serial.print("Angle: "); Serial.println(GPS.angle);
-      Serial.print("Altitude: "); Serial.println(GPS.altitude);
-      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
-    }
+    timer = millis();
   }
+  if (millis() - timer > 1000) 
+  {
+    //print_gps();
+    print_lat();
+    timer = millis(); // reset the timer
+  }
+
+  
 }
 
 
