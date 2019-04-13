8b47c1945941936976ef79aa0ebf20c92cae074d
Tyler Lewis <tylerlewis182@gmail.com>
2019-04-13 02:28:04 -0500

    need to add fix data and parse gprmc and gpgga sentences

--- arduino_scripts/GPS_read_function/GPS.h
+++ arduino_scripts/GPS_read_function/GPS.h
@@ -1,53 +1,100 @@
 #ifndef GPS_H
 #define GPS_H
+#include <Arduino.h>
 
 #define PMTK_SET_NMEA_OUTPUT_RMCGGA "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28"   ///< turn on GPRMC and GGA
 #define PMTK_SET_NMEA_OUTPUT_ALLDATA "$PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0*28"  ///< turn on ALL THE DATA
 #define PGCMD_NOANTENNA "$PGCMD,33,0*6D" // turn off antenna status 
 
+#define GPSSerial Serial3
 
 class GPS
 {
 public:
 
 	// variables
-	HardwareSerial GPSSerial;
-	const long baud; // bits per second
-	const int timeout;     // milliseconds (may need to increase this if data is dropped)
-	const int num_chars;  // this should be the max number of chars in a NMEA sentence
+	long baud; // bits per second
+	int timeout;     // milliseconds (may need to increase this if data is dropped)
+	int num_chars;  // this should be the max number of chars in a NMEA sentence
+	String empty_string = "";
 	String line = "";   // stores the most recent line of data read from serial
+	String gpgga = "";  // holds last GPGGA nmea sentence
 	String gprmc = "";  // holds last GPRMC nmea sentence
-
+	bool gpgga_updated = false;
+	bool gprmc_updated = false;
+	bool updated = false;
 
 
 
 	// constructor
-	GPS(HardwareSerial &GPSSerial, const long baud=9600, const int timeout=100)
+	GPS(long baud=9600, int timeout=100)
 	{
-		this->GPSSerial = GPSSerial;
 		this->baud = baud;
 		this->timeout = timeout;
-		this->num_chars = 256;
+		this->num_chars = 512;
 		this->line = "";
+		this->gpgga = "";
 		this->gprmc = "";
-
+		
 		// initialize serial communication with the GPS
-		this->GPSSerial.begin(baud);
+		GPSSerial.begin(baud);
 
 		// set the timeout for gps serial
-	  this->GPSSerial.setTimeout(timeout); // default, if this line commented out, is 1000 milliseconds
+	  GPSSerial.setTimeout(timeout); // default, if this line commented out, is 1000 milliseconds
   
 		// set gps to only output RMC and GGA lines
-	  this->GPSSerial.println(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+	  GPSSerial.println(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+	  delay(10);
 
 	  // turn off printing antennae status
-	  this->GPSSerial.println(PGCMD_NOANTENNA);
+	  GPSSerial.println(PGCMD_NOANTENNA);
+	  delay(10);
 
 	 	// reserve space for strings
 	  this->line.reserve(num_chars); 
 	  this->gprmc.reserve(num_chars);
+	}
+
+	// get nmea sentences
+	bool get_nmea_sentences()
+	{
+		updated = false; // reset flag
+	  int num_nmea_sentences = 0;
+	  if(GPSSerial.available() > 0)
+	  {
+	    line = GPSSerial.readStringUntil('\n');
+      if(line.startsWith("$GPGGA"))
+      {
+        gpgga = line;
+        gpgga_updated = true;
+      }
+      else if(line.startsWith("$GPRMC"))
+      {
+      	gprmc = line;
+      	gprmc_updated = true;
+      }
+
+      // check if both nmea sentences have been updated
+      if(gpgga_updated && gprmc_updated)
+      {
+      	updated = true; // set flag
+      	gpgga_updated = false; // reset flag
+      	gprmc_updated = false; // reset flag
+      	return true;
+      }
+	  }
+	  else
+	  {
+	  	return false;
+	  }
+	}
 
 
+	// print all member variables
+	print()
+	{
+		Serial.println("print function called...");
+		Serial.flush();
 	}
 
 
--- arduino_scripts/GPS_read_function/main.cpp
+++ arduino_scripts/GPS_read_function/main.cpp
@@ -1,137 +1,43 @@
-#include "Adafruit_GPS.h"
-
-
-// GPS board is connected to this Arduino Mega Serial3 port (pins 14, 15)
-#define GPSSerial Serial3
-
-// Connect to the GPS on the hardware port
-Adafruit_GPS GPS(&GPSSerial);
-
-// global variables
-const long MONITOR_BAUDRATE = 115200; // bits per second
-
-// timer for debugging
-uint32_t timer = millis();
-//uint32_t counter = 0;
-
-
-// functions
-void update_gps()
-{
-  // read data from the GPS in the 'main loop'
-  char c = GPS.read();
-
-  if (GPS.newNMEAreceived())
-  {
-    // print the las recieved NMEA sentence
-    //Serial.println(GPS.lastNMEA()); // this sets the newNMEAreceived() flag to false
-    if (!GPS.parse(GPS.lastNMEA()))   // this sets the newNMEAreceived() flag to false
-    {
-      return;
-    }
-  }  
-}
-
-void print_gps()
-{
-  // approximately every 2 seconds or so, print out the current stats
-  //timer = millis(); // reset the timer
-  //Serial.print("Counter = ");
-  //Serial.println(counter);
-  //counter = 0; // reset the counter
-  Serial.print("\nTime: ");
-  Serial.print(GPS.hour, DEC); Serial.print(':');
-  Serial.print(GPS.minute, DEC); Serial.print(':');
-  Serial.print(GPS.seconds, DEC); Serial.print('.');
-  Serial.println(GPS.milliseconds);
-  Serial.print("Date: ");
-  Serial.print(GPS.day, DEC); Serial.print('/');
-  Serial.print(GPS.month, DEC); Serial.print("/20");
-  Serial.println(GPS.year, DEC);
-  Serial.print("Fix: "); Serial.print((int)GPS.fix);
-  Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
-  if (GPS.fix) 
-  {
-    Serial.print("Location: ");
-    Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-    Serial.print(", ");
-    Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-    Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-    Serial.print("Angle: "); Serial.println(GPS.angle);
-    Serial.print("Altitude: "); Serial.println(GPS.altitude);
-    Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
-  }
-}
-
-void check_type(int a)
-{
-  Serial.println("it's an int");
-}
-
-void print_lat()
-{
-  //check_type(GPS.latitude);
-  Serial.println(GPS.latitude);
-}
-
+#include <Arduino.h>
+#include "GPS.h"
 
 
 // setup
 void setup()
 {
-  Serial.begin(MONITOR_BAUDRATE); // bits per second
-  Serial.println("Program started...");
-
-  GPS.begin(9600);
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); // only send RMC and GGA
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
-  GPS.sendCommand(PGCMD_NOANTENNA);
-
-  delay(2000);
+	init();
+	Serial.begin(115200);
+	Serial.println("Program started...");
+	Serial.println("---");
+	delay(1000);
 }
-
 
 
 
 
 // main
-void loop()
+int main()
 {
-  // read data from the GPS in the 'main loop'
-  update_gps();
-  //counter++; // count how many times the read() method is called (for debugging)
+	// setup timers and serial
+	setup();
 
+	// create a gps object
+	GPS gps;
 
-  // print gps data every few seconds
-  if(timer > millis()) // if timer rolls over, reset it
-  {
-    timer = millis();
-  }
-  if (millis() - timer > 1000) 
-  {
-    //print_gps();
-    print_lat();
-    timer = millis(); // reset the timer
-  }
+	while(true)
+	{
+		// update gps data
+		gps.get_nmea_sentences();
+		if(gps.updated)
+		{
+			Serial.println(gps.gpgga);
+			Serial.flush();
+			Serial.println(gps.gprmc);
+			Serial.flush();
+			Serial.println("---");
+			Serial.flush();
+		}
+		
+	}
 
-  
 }
-
-
-
-
-
-/* serial monitor outputs the following every 1 second:
-   
-  $PGTOP,11,2*6E
-  $GPGGA,234217.000,3342.7293,N,09635.6730,W,1,07,1.15,197.9,M,-24.7,M,,*55
-  $GPGLL,3342.7293,N,09635.6730,W,234217.000,A,A*48
-  $GPGSA,A,3,02,17,28,24,12,06,19,,,,,,1.45,1.15,0.88*02
-  $GPGSV,3,1,11,06,69,203,20,19,63,348,29,17,53,033,29,28,44,112,19*73
-  $GPGSV,3,2,11,02,33,218,17,24,30,296,25,03,15,055,,12,07,311,25*7F
-  $GPGSV,3,3,11,22,03,038,,30,02,166,,46,,,27*76
-  $GPRMC,234217.000,A,3342.7293,N,09635.6730,W,1.14,171.84,060419,,,A*7A
-  $GPVTG,171.84,T,,M,1.14,N,2.11,K,A*30
-
-*/
- 
